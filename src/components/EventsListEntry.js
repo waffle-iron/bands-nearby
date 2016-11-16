09a9225fb6ac8461857d02f01a953d8f09b09ebd
Jen J Wong <jenjwong@gmail.com>
2016-11-15 18:40:50 -0800

    Adds sounds like to dashboard

--- src/components/EventsListEntry.js
+++ src/components/EventsListEntry.js
@@ -22,10 +22,14 @@ class EventsListEntry extends Component {
   }
 
   render() {
-    const { title, link, date, venue, cost, photo, startTime, youTube } = this.props;
+    const { title, link, date, venue, cost, photo, startTime, youTube, similarArtists } = this.props;
     let isDisplayed = this.addClass()
+
+    let soundsLike;
+    similarArtists[0] ? soundsLike = `Sounds like: ${similarArtists}` : null
+
     return (
-      <li className="event-list-entry" onClick={()=> this.toggle()}>
+      <li className="event-list-entry"  onClick={()=> this.toggle()}>
         <div className="show-info">
           <div className="date">{date}</div>
           <span className="title">{title}</span>
@@ -36,12 +40,15 @@ class EventsListEntry extends Component {
           </div>
           <span className="startTime">{startTime}pm</span>
           <span className="cost">${cost}</span>
+          <div className="similarArtists">
+          {soundsLike}
+          </div>
         </div>
         <div className="concert-photo-wrapper">
           <img className="concert-photo" src={photo} alt="concert photo" />
         </div>
         <div className="video-container" className={isDisplayed}>
-          {youTube.map(video => <Video video={video} />)}
+          {youTube.map(video => <Video video={`http://www.youtube.com/embed/${video}`} key={video}/>)}
         </div>
       </li>
     )}

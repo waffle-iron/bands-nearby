51163746b4dbac00c350f6c63be3ff01ef11ac21
Jen J Wong <jenjwong@gmail.com>
2016-11-21 16:28:51 -0800

    Filters YouTube results to removed undefined videos

--- server/workers/youTube.js
+++ server/workers/youTube.js
@@ -5,11 +5,16 @@ const getYouTube = (concertTitle, concertObj) => {
   const searchTerm = concertTitle.split(' ').join('%22');
   const URL = `https://www.googleapis.com/youtube/v3/search?key=${APIKey.YOUTUBE}&part=snippet&maxResults=9&q=%22${searchTerm}%22music%22live%22&{videoEmbeddable:true}`;
  return fetchURL(URL)
-  .then(function(video) {
-    return video.items.map(vid => {
-      return vid.id.videoId
-    })
-  })
+ .then(function(video) {
+     return video.items.filter((vid) => {
+       return vid.id.videoId
+     })
+   })
+   .then(function(videos) {
+     return videos.map(vid => {
+       return vid.id.videoId
+     })
+   })
   .then(function(youTubeArray) {
     concertObj.youTube = youTubeArray.slice(0, 2)
     return concertObj

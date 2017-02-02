168118c1317166d26fc014b39dee5179b5b2724c
Jen J Wong <jenjwong@gmail.com>
2017-02-01 23:13:21 -0800

    refactors for readability

--- server/workers/sf/amnesia.js
+++ server/workers/sf/amnesia.js
@@ -1,33 +1,12 @@
 const jsdom = require('jsdom');
 
-
-
-// { photo: 'http://www.commodoreballroom.com/wp-content/uploads/2016/09/Wintersleep-Photo-230x170.jpg',
-//     link: 'http://www.commodoreballroom.com/calendar/',
-//     venue: 'Commodore Ballroom',
-//     date: 'Saturday, November 19, 2016',
-//     title: 'Fake Palms',
-//     cost: '25',
-//     startTime: '9:30',
-//     youTube: [ 'jkgDvLA4kYw', '5u_vXkQg0OI' ],
-//     similarArtists:
-//      [ 'Greys',
-//        'Corsica Arts Club',
-//        'Mike Krol',
-//        'HSY',
-//        'White Reaper' ],
-//     artistSummary: 'Fake Palms are a Toronto-based band signed to Buzz Records' },
-
-
-
-
 const amnesiaScraper = (window) => {
   const events = window.document.querySelectorAll('.tribe_events');
   const eventsArray = Array.from(events);
 
   return eventsArray.map((event) => {
-    data = event.getAttribute('data-tribejson')
-    scrapedData = JSON.parse(data);
+    const data = event.getAttribute('data-tribejson');
+    const scrapedData = JSON.parse(data);
 
     const reClosed = /Closed/gi;
     if (reClosed.test(scrapedData.title)) {
@@ -44,11 +23,11 @@ const amnesiaScraper = (window) => {
 
     const reStartTime = /(\d+):(\d+)/g;
     let startTime = reStartTime.exec(scrapedData.startTime);
-    startTime  = startTime[1];
+    startTime = startTime[1];
 
-    const reDate = /.*(?=\s@)/gi
+    const reDate = /.*(?=\s@)/gi;
     let date = reDate.exec(scrapedData.startTime);
-    date  = date[1];
+    date = date[1];
 
     const show = {};
     show.title = title;
@@ -56,7 +35,7 @@ const amnesiaScraper = (window) => {
     show.startTime = startTime;
     show.cost = cost;
     show.artistSummary = scrapedData.excerpt;
-    show.link = scrapedData.permalink
+    show.link = scrapedData.permalink;
     return show;
   });
 };

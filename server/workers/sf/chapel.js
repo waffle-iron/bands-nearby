c72d452cc662555068887d8e77eec709fa207559
Jen J Wong <jenjwong@gmail.com>
2017-02-03 17:46:50 -0800

    refactor artist summary scraping

--- server/workers/sf/chapel.js
+++ server/workers/sf/chapel.js
@@ -3,7 +3,6 @@ const getShows = require('../scraper.js');
 const bluebirdPromise = require('bluebird');
 
 const chapelScraper = (window) => {
-
   const getSummaryLinks = (window) => {
     const summaryLinkElements = window.document.querySelectorAll('a.ds-listing-event-title.url.summary');
     const summaryLinkElementsArray = Array.from(summaryLinkElements);
@@ -25,8 +24,14 @@ const chapelScraper = (window) => {
         const reTitle = /(.*):/g;
         let title = reTitle.exec(scrapedTitle);
         title === null ? title = scrapedTitle : title = title[1];
+        if (title.includes(',')) {
+          title = title.split(',')
+        } else {
+          title = [title];
+        }
 
-        let headliner = title.split(',')[0];
+        let headliner = title[0];
+
 
         let scrapedPhoto = window.document.querySelector('.ds-cover-image');
         let scrapedPhotoURL = scrapedPhoto.style['background-image'];
@@ -37,7 +42,7 @@ const chapelScraper = (window) => {
         const scrapedCost = window.document.querySelector('.ds-ticket-info').textContent;
         const reCost = /\$(\d+)/g;
         let cost = reCost.exec(scrapedCost);
-        cost === null ? cost = scrapedCost : cost = cost[1];
+        cost === null ? cost = parseInt(scrapedCost) : cost = parseInt(cost[1]);
 
         const scrapedStartTime = window.document.querySelector('.ds-event-time');
         const reStartTime = /(\d+):(\d+)/g;
@@ -49,15 +54,29 @@ const chapelScraper = (window) => {
         let date = reDate.exec(scrapedDate);
         date === null ? date = date : date = date[1];
 
-        const scrapedArtistSummary = window.document.querySelector('.ds-event-description-inner p').textContent;
-        const artistSummary = scrapedArtistSummary;
+        var scrapedArtistSummary = window.document.querySelectorAll('.ds-event-description-inner p');
+        var paragraphArray = Array.from(scrapedArtistSummary)
+        var firstParagraph = paragraphArray.reduce((acc, item) => {
+          if (!acc[0] && item.textContent) {
+            acc.push(item.textContent)
+          }
+          return acc;
+        }, [])
+        if (firstParagraph[0]) {
+          scrapedArtistSummary = firstParagraph[0];
+        } else {
+          scrapedArtistSummary = window.document.querySelector('.ds-event-description-inner').textContent;
+        }
+
+
+        const artistSummary = scrapedArtistSummary.replace(title[0], '').trim()
 
         const scrapedLink = window.document.querySelector('.ds-buy-tix.tickets');
         let link = scrapedLink;
         scrapedLink === null ? link = null : link = link.href;
 
         const show = {};
-        show.venue = "The Chapel";
+        show.venue = 'The Chapel';
         show.title = title;
         show.headliner = headliner;
         show.date = date;
@@ -68,8 +87,8 @@ const chapelScraper = (window) => {
         show.link = link;
         return show;
       });
-    })
-  }
+    });
+  };
 
   const overviewURL = getSummaryLinks(window);
   return scrapeEachLink(overviewURL);

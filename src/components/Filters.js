39559aa94d6b90d078ee2fc0eabd3daa122d01dd
Jen J Wong <jenjwong@gmail.com>
2017-02-15 21:32:44 -0800

    rudamentary implementation of bound filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,37 +1,59 @@
-import React, {Component} from 'react';
-import {findMinMax, costMatches, typeaheadMatches, filteredMatches, displayMatches} from '../utilities/filterHelpers';
-import debounce from 'lodash/debounce';
+import React, { Component } from 'react';
+import { findMinMax, costMatches, typeaheadMatches } from '../utilities/filterHelpers';
 
 class Filters extends Component {
   constructor() {
     super();
-    this.state = {
-      min: 0,
-      max: 0,
-      typeAheadString: '',
-    };
+    this.state = {};
+  }
+  componentWillMount() {
+    this.setMinMax(this.props.concertData);
   }
   componentDidMount() {
-    const maxMin = this.handleMinMax()
-    this.setState({searchedCost: maxMin[1]});
+    this.rangeInput.value = this.state.max;
   }
 
-  handleMinMax = () => {
-    const maxMin = findMinMax(this.props.concertData);
-    this.setState({min: maxMin[0]});
-    this.setState({max: maxMin[1]});
+  setMinMax = (concerts) => {
+    const maxMin = findMinMax(concerts);
+    this.setState({ min: maxMin[0] });
+    this.setState({ max: maxMin[1] });
     return maxMin;
   }
 
-  handleUpdate = (e) => {
-    console.log('handler caled')
-    displayMatches(this.props.concertData, this.state.typeAheadString, this.state.searchedCost, this.props.handleFilters);
+  minCostCorrection = (concertsMinCost, searchedCost) => {
+    if (concertsMinCost > searchedCost) {
+      this.setState({ searchedCost: concertsMinCost });
+    }
+  }
+
+  getFilteredConcerts = (...args) => {
+    const [concerts, typeAheadString, searchedCost] = [...args];
+    const filterByTypeahead = typeaheadMatches(this.props.concertData, this.state.typeAheadString);
+    console.log(typeAheadString)
+    this.setMinMax(filterByTypeahead);
+    return costMatches(filterByTypeahead, this.state.searchedCost)
+  }
+
+  boundFilter = (value) => {
+    return (...args) => {
+      const [concerts, typeAheadString, searchedCost] = [...args];
+      const filterByTypeahead = typeaheadMatches(this.props.concertData, this.state.typeAheadString);
+      console.log(typeAheadString)
+      this.setMinMax(filterByTypeahead);
+      return costMatches(filterByTypeahead, value)
+    }
+  }
+
+  handleUpdate = (val) => {
+    const test = this.boundFilter(val)
+
+    this.props.handleFilters(test(this.state.concertData, this.state.typeAheadString, this.state.searchedCost));
+    this.minCostCorrection(this.state.min, this.state.searchedCost);
   }
 
   handleInput = (e) => {
-    this.setState({[e.target.name]: e.target.value})
-    this.handleUpdate();
-
+    this.setState({ [e.target.name]: e.target.value });
+    this.handleUpdate(e.target.value);
   }
 
   render() {
@@ -40,7 +62,15 @@ class Filters extends Component {
       <div>
         <label>Search</label>
         <input name="typeAheadString" type="text" onChange={(e)=>this.handleInput(e)} onKeyUp={(e)=>this.handleUpdate(e)} placeholder="Band/SoundsLike/Venue"/>
-        $<input name="searchedCost" type="range"  onChange={(e)=>this.handleInput(e)} onTouchEnd={(e)=>this.handleInput(e)} onMouseUp={(e)=>this.handleInput(e)} min={this.state.min} max={this.state.max} value={this.state.searchedCost}/>$$$
+        {this.state.min !== Infinity &&
+        <div>
+          <div>
+            ${this.state.searchedCost}
+          </div>
+          ${this.state.min}
+          <input name="searchedCost" type="range"  onChange={(e)=>this.handleInput(e)} onTouchEnd={(e)=>this.handleInput(e)} onMouseUp={(e)=>this.handleInput(e)} min={this.state.min} max={this.state.max} ref={(input) => { this.rangeInput = input; }}/>
+          ${this.state.max}
+        </div>}
       </div>
     )
   }

5bcb192062a16109198ec0d6b0cd7ab08c9e47d7
Jen J Wong <jenjwong@gmail.com>
2017-03-01 15:55:50 -0800

    refactors chronjob

--- server/workers/chronJob.js
+++ server/workers/chronJob.js
@@ -1,10 +1,9 @@
 const getShows = require('./scraper.js');
 const getYouTube = require('./youTube.js');
 const getSimilarArtists = require('./lastFm');
-const sfScraper = require('./sf/chapel');
+const sfScraper = require('./sf/bayBridged');
 const getGoogleSearchResult = require('./googleSearchResult');
 const fs = require('fs');
-const chalk = require('chalk');
 
 
 const cleanData = (data) => {
@@ -17,7 +16,7 @@ const cleanData = (data) => {
 };
 
 const generateShowData = (venues) => {
-  fs.rename('bandsNearbyData.json', JSON.stringify(new Date()));
+  fs.rename('../data/bandsNearbyData.json', '../data/archived/' + JSON.stringify(new Date()) + '.json');
   const allData = [];
   venues.forEach((venue, index, collection) => {
     setTimeout(function() {
@@ -35,7 +34,7 @@ const generateShowData = (venues) => {
       .then(function(data) {
         if (index === collection.length - 1) {
           const stringifyedData = JSON.stringify(data, null, ' ');
-          fs.appendFile('bandsNearbyData.json', stringifyedData, 'utf8')
+          fs.appendFile('../data/bandsNearbyData.json', stringifyedData, 'utf8')
         }
       })
       .catch((data) => console.log(`Handle error: error in chronjob.js + ${concertData}`))
@@ -44,7 +43,7 @@ const generateShowData = (venues) => {
 }
 
 const venues = [
-  { name: 'Chapel', http: 'http://calendar.thebaybridged.com/venues/the-chapel?page=1' },
+  { name: 'The Chapel', http: 'http://calendar.thebaybridged.com/venues/the-chapel?page=1' },
   { name: 'Fox Theater', http: 'http://calendar.thebaybridged.com/venues/fox-theater' },
 ];
 

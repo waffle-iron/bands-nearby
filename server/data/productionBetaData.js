95f2c409c43a9c438f0bc11d55fa616c0c176d0c
Jen J Wong <jenjwong@gmail.com>
2017-03-07 14:44:06 -0800

    refactors relevantShows to return data for the week

--- server/data/productionBetaData.js
+++ server/data/productionBetaData.js
@@ -12,8 +12,10 @@ console.log(dayNow, 'DAY NOOOOWW')
 const relevantData = (data) => {
   return data.filter((item) => {
     const dateArray = item.date.split('-');
-    if (dateArray[1] == monthNow && dateArray[2] > dayNow && dateArray[2] <= dayNow + 6 && item.youTube !== [] && item.similarArtists !== []) {
-      console.log(dateArray[2])
+    if (dateArray[1] == monthNow && dateArray[2] > dayNow && dateArray[2] <= (dayNow + 7) && item.youTube !== [] && item.similarArtists !== []) {
+      if (item.cost === 0) {
+        item.cost = 'No Cover';
+      }
       return true;
     }
   });

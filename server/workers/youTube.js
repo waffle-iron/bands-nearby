b5ddb15fc4dab6e1d2c894c41f9979cef4b62df0
Jen J Wong <jenjwong@gmail.com>
2017-03-06 23:32:48 -0800

    refactors youTube.js to use reduce

--- server/workers/youTube.js
+++ server/workers/youTube.js
@@ -2,26 +2,30 @@ const fetchURL = require('./fetch');
 const APIKey = require('./APIKeys');
 
 const getYouTube = (concertTitle, concertObj) => {
-  let searchTerm = concertTitle[0].trim().split(' ').join('%22');
-  const URL = `https://www.googleapis.com/youtube/v3/search?key=${APIKey.YOUTUBE}&part=snippet&maxResults=9&q=%22${searchTerm}%22music%22live%22&{videoEmbeddable:true}`;
- return fetchURL(URL)
- .then(function(youTubeArtistSummary) {
-     return youTubeArtistSummary.items.filter((videos) => {
-       return videos.id.videoId[0]
-     })
-   })
-   .then(function(videos) {
-     return videos.map(video => {
-       return video.id.videoId
-     })
-   })
-  .then(function(videos) {
-    concertObj.youTube = videos.slice(0, 2)
-    return concertObj
+  let searchTerm = concertTitle[0].trim().split(' ').join('%20');
+  searchTerm = searchTerm.replace(/’s/g, '');
+  searchTerm = searchTerm.replace(/’/g, '');
+  searchTerm = searchTerm.replace(/"/g, '');
+  searchTerm = searchTerm.toString();
+  const URL = `https://www.googleapis.com/youtube/v3/search?key=${APIKey.YOUTUBE}&part=snippet&maxResults=9&q=%22${searchTerm}%22music%22live&{videoEmbeddable:true}`;
+  return fetchURL(URL)
+  .then((youTubeArtistSummary) => {
+    return youTubeArtistSummary.items.reduce((acc, item) => {
+      if (item.id.videoId) {
+        acc.push(item.id.videoId);
+      }
+      return acc;
+    }, []);
   })
-  .catch(() => {
-    console.log('Error Handling: youTube.js error', searchTerm)
+  .then((videos) => {
+    concertObj.youTube = videos.slice(0, 2);
+    return concertObj;
+  })
+  .catch((data) => {
+    console.error('Error Handling: youTube.js error', searchTerm);
+    concertObj.youTube = [];
+    return concertObj;
   });
-}
+};
 
 module.exports = getYouTube;

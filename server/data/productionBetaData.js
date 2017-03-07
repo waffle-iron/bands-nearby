d72397cc4c11e5fc341b6fc881923ebe8b4a0df9
Jen J Wong <jenjwong@gmail.com>
2017-03-07 02:55:38 -0800

    filters for current day

--- server/data/productionBetaData.js
+++ server/data/productionBetaData.js
@@ -1,17 +1,19 @@
 // programatically filters shows for today's concerts
 
-const data = require('./bandsNearbyData.json');
-// const data = require('./bandsNearbyDataMock');
+// const data = require('./bandsNearbyData.json');
+const data = require('./bandsNearbyDataMock');
 
 
 const today = new Date();
 const dayNow = today.getDate();
 const monthNow = today.getMonth() + 1;
+console.log(dayNow, 'DAY NOOOOWW')
 
 const relevantData = (data) => {
   return data.filter((item) => {
     const dateArray = item.date.split('-');
-    if (dateArray[1] == monthNow && dateArray[2] >= dayNow && dateArray[2] <= dayNow + 6 && item.youTube !== [] && item.similarArtists !== []) {
+    if (dateArray[1] == monthNow && dateArray[2] > dayNow && dateArray[2] <= dayNow + 6 && item.youTube !== [] && item.similarArtists !== []) {
+      console.log(dateArray[2])
       return true;
     }
   });

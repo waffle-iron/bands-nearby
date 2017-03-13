57831495dec00d09a45f8ff8339d01a7ebfd1bcc
Jen J Wong <jenjwong@gmail.com>
2017-03-13 01:10:37 -0700

    updates javascript date object with getUTCDay method for correct time zone

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -2,7 +2,8 @@ export const isSmallScreen = () => !window.matchMedia("(max-width: 667px)").matc
 
 export const dayToEnglish = (date) => {
   const dateObj = new Date(date);
-  const day = dateObj.getDay();
+  let day = dateObj.getUTCDay();
+
   const numToWord = {
     0: 'Sunday',
     1: 'Monday',
@@ -11,6 +12,8 @@ export const dayToEnglish = (date) => {
     4: 'Thursday',
     5: 'Friday',
     6: 'Saturday',
-  }
+  };
   return numToWord[day];
-}
+};
+
+export const isFree = (price) => price == 0 ? "No Cover" : `Price $${price}`;

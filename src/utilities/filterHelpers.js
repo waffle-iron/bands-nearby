9e1338e0cc66e1c130ffc7519480abc70855270d
Jen J Wong <jenjwong@gmail.com>
2017-03-17 13:13:43 -0700

    removes relaventShow function

--- src/utilities/filterHelpers.js
+++ src/utilities/filterHelpers.js
@@ -48,36 +48,4 @@ export const displayMatches = (concertData, typeAheadSearch, costSearch, handleF
   return handleFilters(filtered);
 };
 
-
-//
-//
-// const relaventShow = (month) => {
-//   let isRelavent = false;
-//   const inputMonth = month.toUpperCase();
-//
-//   const monthMap = {
-//     0: 'JAN',
-//     1: 'FEB',
-//     2: 'MAR',
-//     3: 'APR',
-//     4: 'MAY',
-//     5: 'JUN',
-//     6: 'JUL',
-//     7: 'AUG',
-//     8: 'SEP',
-//     9: 'OCT',
-//     10: 'NOV',
-//     11: 'DEC',
-//   };
-//
-//   const date = new Date();
-//   const curMonthNum = date.getMonth();
-//   const currentMonth = monthMap[curMonthNum];
-//   const nextMonth = monthMap[curMonthNum + 1];
-//
-//   if (inputMonth === currentMonth || inputMonth === nextMonth) {
-//     isRelavent = true;
-//   }
-//
-//   return isRelavent;
-// };
+export const displayMin = min => min === 0 ? 'Free' : `$${min}`;

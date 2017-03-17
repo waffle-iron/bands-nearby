5ce9c1411a62270d069473973a415e04bbaea700
Jen J Wong <jenjwong@gmail.com>
2017-03-17 13:15:02 -0700

    refactors slide-in

--- src/components/EventsListEntry.js
+++ src/components/EventsListEntry.js
@@ -1,94 +1,72 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
-
-import SimilarArtistsList from './SimilarArtistsList'
-import TitleList from './TitleList'
+import SimilarArtistsList from './SimilarArtistsList';
+import TitleList from './TitleList';
 import InfoDropDown from './InfoDropDown';
-import { dayToEnglish } from '../utilities/utils.js';
+import { dayToEnglish, isCover, textClamp } from '../utilities/utils';
 
 class EventsListEntry extends Component {
   constructor() {
     super();
     this.state = {
       toggled: false,
-      ele: null,
+      isClicked: 'raised',
     };
   }
 
-  componentDidMount() {
-    window.document.addEventListener('scroll', () => this.isVisible(this.state.ele), true);
-    this.setState({ele: ReactDOM.findDOMNode(this)});
+  setIsClicked() {
+    if (this.state.isClicked === 'raised') {
+      this.setState({ isClicked: 'lowered' });
+      return;
+    }
+    this.setState({ isClicked: 'raised' });
   }
+
   toggle(e) {
-    if (e.target.tagName.toLowerCase() === "a") {
+    if (e.target.tagName.toLowerCase() === 'a') {
       return;
     }
     this.setState({ toggled: !this.state.toggled });
   }
-  addClass() {
-    return this.state.toggled ? 'show ' : 'hide';
-  }
-  isCover(cost) {
-    return cost !== 0 && cost === cost ? cost = `$${cost}` : cost = 'No Cover';
-  }
 
-  isVisible = (el) => {
-     if (el){
-       const elemTop = el.getBoundingClientRect().top;
-       const elemBottom = el.getBoundingClientRect().bottom;
-       const isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-      //  console.log(isVisible, this.props.titles[0])
-       return isVisible;
-     }
+  clickHandler(e) {
+    this.setIsClicked();
+    this.toggle(e);
   }
-
-// bands title must be less than six words
-  handleSmallScreen = (headliner) => {
-    if (!window.matchMedia("(min-width: 667px)").matches) {
-      return headliner.split(' ').slice(0, 3).join(' ');
-    }
-    return headliner.split(' ').slice(0, 5).join(' ');
-
-  }
-
 
   render() {
-    const { titles, ticketLink, date, venue, cost, photo, startTime, youTube, similarArtists, artistSummary, id, exploreMusic, eMusicHandler } = this.props;
-    const headliner = this.handleSmallScreen(titles[0]);
-    const isDisplayed = this.addClass();
-    const showCost = this.isCover(cost);
-    const day = dayToEnglish(date);
+    const { titles, ticketLink, date, venue, cost, photo, startTime, youTube, similarArtists, artistSummary, id } = this.props;
+    const showCost = isCover(cost);
     return (
       <li className="event-list-entry">
         <div className="show-info-container">
-        <div className="show-info"  onClick={e => this.toggle(e)}>
-          <div className="hover-hilight">
-            {showCost !== 'No Cover' && <div className="tickets"><a href={ticketLink}>Tickets<span className="cost">{showCost}</span></a></div>}
-            {showCost === 'No Cover' && <div className="tickets"><span className="cost">{showCost}</span></div>}
-            <div className="date">{day}</div>
-            <div className="headliner">{headliner}</div>
-            <span className="other-bands"><TitleList titles={this.props.titles} /></span>
-            <div>
-              <span className="venue">{venue}</span>
-              <span className="startTime">{startTime}pm</span>
+          <div className="show-info" onClick={e => this.clickHandler(e)} >
+            <div className="hover-hilight">
+              {showCost !== 'No Cover' && <div className="tickets"><a href={ticketLink}>Tickets<span className="cost">{showCost}</span></a></div>}
+              {showCost === 'No Cover' && <div className="tickets"><span className="cost">{showCost}</span></div>}
+              <div className="date">{dayToEnglish(date)}</div>
+              <div className="headliner">{textClamp(titles[0])}</div>
+              <span className="other-bands">
+                <TitleList titles={this.props.titles} />
+              </span>
+              <div>
+                <span className="venue">{venue}</span>
+                <span className="startTime">{startTime}pm</span>
+              </div>
+            </div>
+            <div className="similar-artists">
+              {similarArtists[0] && <SimilarArtistsList artists={similarArtists} />}
             </div>
           </div>
-          <div className="similar-artists">
-            {similarArtists[0] && <SimilarArtistsList artists={similarArtists} />}
-          </div>
         </div>
-      </div>
         <div className="concert-photo-wrapper" >
           <img className="concert-photo" src={photo} alt={photo} />
         </div>
         <InfoDropDown
-          eMusicHandler={eMusicHandler}
           id={id}
           key={id}
           artistSummary={artistSummary} photo={photo}
           youTube={youTube} toggled={this.state.toggled}
-          isDisplayed={isDisplayed}
-          exploreMusic={exploreMusic}
+          slideAnimation={this.state.isClicked}
         />
       </li>
     );
--- src/components/InfoDropDown.js
+++ src/components/InfoDropDown.js
@@ -1,40 +1,38 @@
-import React, {Component} from 'react';
-import VideoList from './VideoList'
+import React, { Component } from 'react';
+import VideoList from './VideoList';
+
 
 class InfoDropDown extends Component {
   constructor() {
     super();
+    this.state = {
+      isToggled: 'hide',
+    };
   }
 
   render() {
-    const { exploreMusic, eMusicHandler, id, artistSummary, photo, youTube, toggled, isDisplayed } = this.props;
-    let style;
-    let isOpen;
-    exploreMusic ? style = 'exploreMusicIndex-more-info-dropdown' : style = 'more-info-dropdown';
-    exploreMusic ? isOpen = true : null;
-    toggled ? isOpen = true : null;
-
-    let slidein;
-    if (!toggled | !isOpen) {
-      slidein = 'raised'
-    } else {
-      slidein = 'lowered';
-    }
-
+    const { id, artistSummary, photo, youTube, toggled, slideAnimation } = this.props;
+    let isShown;
+    toggled ? isShown = 'show' : isShown = 'hide';
     return (
       <div className="dropdown-container">
-      <div className={slidein}>
-      <div className={style} className={isDisplayed} >
-        <div className="artist-summary-container">
-          <div className="artist-summary">
-            <p>{artistSummary}</p>
+        <div className={slideAnimation} >
+          <div className={isShown}>
+            <div className="artist-summary-container">
+              <div className="artist-summary">
+                <p>{artistSummary}</p>
+              </div>
+            </div>
+            <VideoList
+              photo={photo}
+              youTube={youTube}
+              isRendered={toggled}
+            />
           </div>
         </div>
-        <VideoList photo={photo} youTube={youTube} isRendered={isOpen} eMusicHandler={eMusicHandler} />
       </div>
-        </div>
-        </div>
     );
   }
-}
+};
+
 export default InfoDropDown;

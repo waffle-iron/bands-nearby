b2191a2d7c23f6ac8d7014b59d5a45c11395be23
Jen J Wong <jenjwong@gmail.com>
2017-03-16 20:01:14 -0700

    adds parameter that allows job to scrape with multiple scrapers

--- server/workers/chronJob.js
+++ server/workers/chronJob.js
@@ -3,7 +3,6 @@ const getYouTube = require('./youTube.js');
 const getSimilarArtists = require('./lastFm');
 const sfScraper = require('./sf/bayBridged');
 const getGoogleSearchResult = require('./googleSearchResult');
-// const fs = require('fs');
 const bluebirdPromise = require('bluebird');
 const util = require('util');
 const fs = bluebirdPromise.promisifyAll(require("fs"));
@@ -11,70 +10,47 @@ const fs = bluebirdPromise.promisifyAll(require("fs"));
 const generateShowData = (venue) => {
   console.log('called with', venue.name)
   getShows(venue.http)
-  .then((data) => sfScraper(data, venue.name))
+  .then((data) => venue.scraper(data, venue.name))
   .map(concert => getYouTube(concert.title, concert))
   .map(concert => getSimilarArtists(concert.title, concert))
-  // .map(concert => getGoogleSearchResult(concert.title, concert))
-  .then(function(data) {
+  .map(concert => getGoogleSearchResult(concert.title, concert))
+  .then((data) => {
     const allData = JSON.parse(fs.readFileSync('../data/bandsNearbyData.json').toString());
-    allData.push(...data)
+    allData.push(...data);
     const stringifyedData = JSON.stringify(allData, null, ' ');
-    console.log(stringifyedData)
+    console.log(stringifyedData);
     fs.writeFile('../data/bandsNearbyData.json', stringifyedData, 'utf8');
   })
-  .catch((data) => console.log(`Handle error: error in chronjob.js + ${data}`))
-}
+  .catch(data => console.error(`Handle error: error in chronjob.js + ${data}`))
+};
 
 const venues = [
-  // { name: 'The Chapel', http: 'http://calendar.thebaybridged.com/venues/the-chapel?page=1'},
-  { name: 'Fox Theater', http: 'http://calendar.thebaybridged.com/venues/fox-theater' },
-  // { name: 'The Fillmore', http: 'http://calendar.thebaybridged.com/venues/the-fillmore' },
-  // { name: 'Lost Church', http: 'http://calendar.thebaybridged.com/venues/the-lost-church' },
-  // { name: 'Great American Music Hall', http: 'http://calendar.thebaybridged.com/venues/great-american-music-hall' },
-  // { name: 'The Independent', http: 'http://calendar.thebaybridged.com/venues/the-independent' },
-  // { name: 'Bimbo\'s 365', http: 'http://calendar.thebaybridged.com/venues/bimbo-s-365-club' },
-  // { name: 'Rickshaw Stop', http: 'http://calendar.thebaybridged.com/venues/rickshaw-stop' },
-  // { name: 'Slim\'s', http: 'http://calendar.thebaybridged.com/venues/slim-s' },
-  // { name: 'The Greek Theater', http: 'http://calendar.thebaybridged.com/venues/greek-theatre' },
-  // { name: 'Sweetwater Music Hall', http: 'http://calendar.thebaybridged.com/venues/sweetwater-music-hall' },
-  // { name: 'Doc\'s Lab', http: 'http://calendar.thebaybridged.com/venues/docs-lab' },
+  { name: 'The Chapel', http: 'http://calendar.thebaybridged.com/venues/the-chapel?page=1', scraper: sfScraper },
+  { name: 'Fox Theater', http: 'http://calendar.thebaybridged.com/venues/fox-theater', scraper: sfScraper },
+  { name: 'The Fillmore', http: 'http://calendar.thebaybridged.com/venues/the-fillmore', scraper: sfScraper },
+  { name: 'Lost Church', http: 'http://calendar.thebaybridged.com/venues/the-lost-church', scraper: sfScraper },
+  { name: 'Great American Music Hall', http: 'http://calendar.thebaybridged.com/venues/great-american-music-hall', scraper: sfScraper  },
+  { name: 'The Independent', http: 'http://calendar.thebaybridged.com/venues/the-independent', scraper: sfScraper },
+  { name: 'Bimbo\'s 365', http: 'http://calendar.thebaybridged.com/venues/bimbo-s-365-club', scraper: sfScraper },
+  { name: 'Rickshaw Stop', http: 'http://calendar.thebaybridged.com/venues/rickshaw-stop', scraper: sfScraper },
+  { name: 'Slim\'s', http: 'http://calendar.thebaybridged.com/venues/slim-s', scraper: sfScraper },
+  { name: 'The Greek Theater', http: 'http://calendar.thebaybridged.com/venues/greek-theatre', scraper: sfScraper },
+  { name: 'Sweetwater Music Hall', http: 'http://calendar.thebaybridged.com/venues/sweetwater-music-hall', scraper: sfScraper },
+  { name: 'Doc\'s Lab', http: 'http://calendar.thebaybridged.com/venues/docs-lab', scraper: sfScraper },
 ];
 
-// { name: 'Yoshi\'s', http: 'http://calendar.thebaybridged.com/venues/yoshi-s-oakland' },
-// { name: 'Davies Symphony Hall', http: 'http://calendar.thebaybridged.com/venues/davies-symphony-hall' },
-// { name: 'DNA Lounge', http: 'http://calendar.thebaybridged.com/venues/dna-lounge' },
-// { name: 'The New Parish', http: 'http://calendar.thebaybridged.com/venues/the-new-parish' },
-// { name: 'Moe\'s Alley', http: 'http://calendar.thebaybridged.com/venues/moe-s-alley' },
-// { name: 'The Hotel Utah Saloon', http: 'http://calendar.thebaybridged.com/venues/the-hotel-utah-saloon' },
-// { name: 'Social Hall', http: 'http://calendar.thebaybridged.com/venues/social-hall-sf' },
-// { name: 'Monarch', http: 'http://calendar.thebaybridged.com/venues/monarch' },
-
-// { name: 'Thee Parkside', http: 'http://calendar.thebaybridged.com/venues/thee-parkside' },
-
-//mediocre
-
-// bad
-// { name: 'The Catalyst', http: 'http://calendar.thebaybridged.com/venues/the-catalyst' },
-// { name: 'Hemlock Tavern', http: 'http://calendar.thebaybridged.com/venues/hemlock-tavern' },
-
-
-// creates bandsNearbyData file if it does not exist, archives the last version
-// scrapes site every 20 seconds for new data
+// creates bandsNearbyData file if it does not exist, archives the last version using node-fs
 const runTask = (venues) => {
   fs.appendFileAsync('../data/bandsNearbyData.json', '', 'utf8')
-  .then(function(data) {
-    fs.rename('../data/bandsNearbyData.json', '../data/archived/' + JSON.stringify(new Date()) + '.json');
-  })
-  .then(function(data) {
-    fs.writeFile('../data/bandsNearbyData.json', '[]', 'utf8');
-  })
-  .then(function(data) {
+  .then(() => fs.rename('../data/bandsNearbyData.json', `../data/archived/${JSON.stringify(new Date())}.json`))
+  .then(() => fs.writeFile('../data/bandsNearbyData.json', '[]', 'utf8'))
+  .then(() => {
     venues.forEach((venue, index) => {
       setTimeout(() => {
         generateShowData(venue);
-      }, 20000 * index)
+      }, 20000 * index * Math.random());
     });
-  })
+  });
 };
 
 runTask(venues);
